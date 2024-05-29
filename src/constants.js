752f6664076957785f7ec9943555c08cde048761
Swarnava Karan <swarnavakaran2001@gmail.com>
2024-05-29 13:46:39 +0530

    1st

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
+    . I have been studying Golang and Java recently. ugh i hate programming! 😞.
+    Here is my <a href="https://github.com/swarnava2001karan" target="_blank">Github</a>!`,
+  "cs-degree": `This is my CS degree.I graduated in 2023 from Msit in W.B. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
+  You'll learn how I built this portfolio you're currently playing through!`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@javascriptmastery" target="_blank">Js Mastery</a>, 
+  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1qKNMGE5pm8q958V_TAXUeFfiBkA4ehwn/view?usp=drive_link" target="_blank">Check it out?</a>
+  Contact me at swarnavakaran@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript. I actually made this taking inspiration from a famous i follow 😜
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  library: `There are a lot of programming books on my shelves. There is even one in Hindi (I also speak Hindi btw).
+  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+  exit: `If you want to exit my portfolio, just close the tab.`,
+};
