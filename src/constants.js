d516be3f75d539afeca6a3979181974e6c0c89b1
Swarnava Karan <swarnavakaran2001@gmail.com>
2024-05-29 13:59:35 +0530

    added text

--- src/constants.js
+++ src/constants.js
@@ -6,16 +6,16 @@ export const dialogueData = {
     Here is my <a href="https://github.com/swarnava2001karan" target="_blank">Github</a>!`,
   "cs-degree": `This is my CS degree.I graduated in 2023 from Msit in W.B. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
   "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
-  You'll learn how I built this portfolio you're currently playing through!`,
+  Are you curious how I made this? 😎`,
   tv: `That's my TV. I've been watching tech youtubers a lot recently like :
    <a href="https://www.youtube.com/@javascriptmastery" target="_blank">Js Mastery</a>, 
   <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
-  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it. And if I have to be honest with you, I am just lazy to code 😔`,
   resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1qKNMGE5pm8q958V_TAXUeFfiBkA4ehwn/view?usp=drive_link" target="_blank">Check it out?</a>
   Contact me at swarnavakaran@gmail.com if you have any interesting job opportunities!`,
-  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript. I actually made this taking inspiration from a famous i follow 😜
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript. I actually made this taking inspiration from a famous web developer i follow 😜
   Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
-  library: `There are a lot of programming books on my shelves. There is even one in Hindi (I also speak Hindi btw).
-  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+  library: `There are a lot of programming books on my shelves. There is even one in Hindi (I also speak Hindi btw). Do You know I know English, Hindi, Bengali and a bit of spanish as well !!😍
+  Aaand - I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
   exit: `If you want to exit my portfolio, just close the tab.`,
 };
